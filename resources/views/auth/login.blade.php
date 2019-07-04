af1f319ef62bff00ccee85d6ea42af7531c50780
daviidy <yaodavidarmel@gmail.com>
2019-07-04 21:56:27 +0000

    adding login and register views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,177 @@
-@extends('layouts.app')
+
+@extends('layouts.menu-login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<div class="limiter">
+  <div class="container-login100">
+    <div class="wrap-login100">
+      <form class="login100-form validate-form">
+        @csrf
+        <span class="login100-form-title p-b-26">
+          Se connecter
+        </span>
+        <span class="login100-form-title p-b-48">
+          <i class="zmdi zmdi-font"></i>
+        </span>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="wrap-input100 validate-input" data-validate = "Un email valide est du type: a@b.c">
+          <input id="email" class="input100 @error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+          <span class="focus-input100" data-placeholder="Email"></span>
+          @error('email')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
         </div>
+
+        <div class="wrap-input100 validate-input" data-validate="Entrer un mot de passe">
+          <span class="btn-show-pass">
+            <i class="zmdi zmdi-eye"></i>
+          </span>
+          <input id="password" class="input100  @error('password') is-invalid @enderror" type="password" name="password" required autocomplete="current-password">
+          <span class="focus-input100" data-placeholder="Mot de passe"></span>
+          @error('password')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
+
+        <div class="wrap-input100 validate-input">
+          <span>
+            <input id="remember" class="input100" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+            <label for="">Se souvenir de moi</label>
+          </span>
+
+        </div>
+
+        <div class="container-login100-form-btn">
+          <div class="wrap-login100-form-btn">
+            <div class="login100-form-bgbtn"></div>
+            <button class="login100-form-btn">
+              Se connecter
+            </button>
+          </div>
+        </div>
+
+        <div class="text-center p-t-115">
+          Nouveau ?
+          </span>
+
+          <a class="txt2" href="#">
+            Inscrivez-vous
+          </a>
+        </div>
+        @if (Route::has('password.request'))
+        <div class="text-center p-t-115">
+          <span class="txt1">
+            <a href="{{ route('password.request') }}">
+              Mot de passe oublié ?
+            </a>
+          </span>
+        </div>
+        @endif
+      </form>
     </div>
+  </div>
 </div>
+
+
+<div id="dropDownSelect1"></div>
+
+
+
+<div class="limiter">
+  <div class="container-login100">
+    <div class="wrap-login100">
+      <form class="login100-form validate-form">
+        @csrf
+        <span class="login100-form-title p-b-26">
+          S'inscrire
+        </span>
+        <span class="login100-form-title p-b-48">
+          <i class="zmdi zmdi-font"></i>
+        </span>
+
+        <div class="wrap-input100 validate-input" data-validate = "Entrer un nom d'utilisateur valide">
+          <input id="name" class="input100 @error('name') is-invalid @enderror" type="text" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+          <span class="focus-input100" data-placeholder="Nom d'utilisateur"></span>
+          @error('name')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
+
+        <div class="wrap-input100 validate-input" data-validate = "Un email valide est du type: a@b.c">
+          <input id="email" class="input100 @error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+          <span class="focus-input100" data-placeholder="Email"></span>
+          @error('email')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
+
+        <div class="wrap-input100 validate-input" data-validate="Entrer un mot de passe">
+          <span class="btn-show-pass">
+            <i class="zmdi zmdi-eye"></i>
+          </span>
+          <input id="password" class="input100  @error('password') is-invalid @enderror" type="password" name="password" required autocomplete="current-password">
+          <span class="focus-input100" data-placeholder="Mot de passe"></span>
+          @error('password')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
+
+        <div class="wrap-input100 validate-input" data-validate="Les mots de passe ne correspondent pas">
+          <span class="btn-show-pass">
+            <i class="zmdi zmdi-eye"></i>
+          </span>
+          <input id="password-confirm" class="input100" type="password" name="password_confirmation" required autocomplete="new-password">
+          <span class="focus-input100" data-placeholder="Confirmer le mot de passe"></span>
+
+        </div>
+
+
+
+        <div class="container-login100-form-btn">
+          <div class="wrap-login100-form-btn">
+            <div class="login100-form-bgbtn"></div>
+            <button class="login100-form-btn">
+              S'inscrire
+            </button>
+          </div>
+        </div>
+
+        <div class="text-center p-t-115">
+          <span class="txt1">
+            Vous avez déjà un compte ?
+          </span>
+
+          <a class="txt2" href="#">
+            COnnectez-vous
+          </a>
+        </div>
+        @if (Route::has('password.request'))
+        <div class="text-center p-t-115">
+          <span class="txt1">
+            <a href="{{ route('password.request') }}">
+              Mot de passe oublié ?
+            </a>
+          </span>
+        </div>
+        @endif
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<div id="dropDownSelect1"></div>
+
+
 @endsection
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,21 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style-homepage.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="/js/index.js" type="text/javascript"></script>
     <script src="/js/jQuery_cookie.js" type="text/javascript"></script>
     <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
   </head>
 <body class="mapflow fr lang-fr brand-zipjet-vanilla home" cz-shortcut-listen="true">
-  <script src="https://connect.facebook.net/signals/config/1900578980161244?v=2.8.51&amp;r=stable" async=""></script>
-  <script src="https://connect.facebook.net/signals/plugins/inferredEvents.js?v=2.8.51" async=""></script>
-  <script src="https://connect.facebook.net/signals/config/1727736534161307?v=2.8.51&amp;r=stable" async=""></script>
-  <script async="" src="https://connect.facebook.net/en_US/fbevents.js"></script>
-  <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
-  <script type="text/javascript" async="" src="//static.hotjar.com/c/hotjar-601883.js?sv=5"></script>
-  <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
-  <script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-5CXD3B"></script>
+
 
   <!-- main container of all the page elements -->
   <div class="mobile-menu">
@@ -148,7 +141,7 @@
                         </ol>
                       </li>
                       <li class="entry login">
-                        <a href="https://www.zipjet.fr/fr/paris/login">
+                        <a href="{{ route('login') }}">
                           Connexion </a>
                       </li>
                     </ol>
@@ -478,238 +471,6 @@
   </div>
 
 
-  <!-- include custom JavaScript -->
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?language=fr&amp;libraries=places&amp;client=gme-zipjetglobalservices&amp;channel=frontend&amp;key=AIzaSyDgYBDIrI7PUpkE6rNC0d7lZw4c9aqylwQ"></script>
 
-  <script src="/compiled/js/frontend.js?bab280f2d0705420357973652030e062" type="text/javascript"></script>
-  <script type="text/javascript" id="">
-    ! function(b, e, f, g, a, c, d) {
-      b.fbq || (a = b.fbq = function() {
-        a.callMethod ? a.callMethod.apply(a, arguments) : a.queue.push(arguments)
-      }, b._fbq || (b._fbq = a), a.push = a, a.loaded = !0, a.version = "2.0", a.queue = [], c = e.createElement(f), c.async = !0, c.src = g, d = e.getElementsByTagName(f)[0], d.parentNode.insertBefore(c, d))
-    }(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-    fbq("init", "1727736534161307");
-    fbq("track", "PageView");
-  </script>
-  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1727736534161307&amp;ev=PageView&amp;noscript=1"></noscript>
-
-  <script type="text/javascript" id="">
-    ! function(b, e, f, g, a, c, d) {
-      b.fbq || (a = b.fbq = function() {
-        a.callMethod ? a.callMethod.apply(a, arguments) : a.queue.push(arguments)
-      }, b._fbq || (b._fbq = a), a.push = a, a.loaded = !0, a.version = "2.0", a.queue = [], c = e.createElement(f), c.async = !0, c.src = g, d = e.getElementsByTagName(f)[0], d.parentNode.insertBefore(c, d))
-    }(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-    fbq("init", "1900578980161244");
-    fbq("track", "PageView");
-  </script>
-  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1900578980161244&amp;ev=PageView&amp;noscript=1"></noscript>
-
-
-  <script type="text/javascript">
-    // page init
-    jQuery(function() {
-      //initOpenClose();
-      initLightbox();
-      jQuery('input, textarea').placeholder();
-      initStickyNav();
-      initParallaxBg();
-      initCycleCarousel();
-      initAnchorLinks();
-      initCitySelector();
-      initPromoBanner();
-      initPromoBottomBanner();
-      new WOW().init();
-      initBackgroundSwitch();
-    });
-  </script>
-
-  <script type="text/javascript">
-    (function($) {
-      $(".match-height").matchHeight();
-    })(jQuery);
-  </script>
-  <script type="text/javascript">
-    $(function($) {
-      var $addressLine = $(".addressLine");
-
-      if (window.innerWidth < 768) {
-        $("input[name=mobile]").val("true");
-
-        $addressLine.pin({
-          localizeMeOnInputFocus: true
-        });
-
-        // This is a fix for Safari form navigation problems:
-        // see https://jira.rocket-internet.de/browse/INTWASH-3488
-        $('div:hidden input').attr('readonly', 'readonly');
-        $('div.lightbox input').attr('readonly', 'readonly');
-      } else {
-        $addressLine.pin();
-      }
-    });
-  </script>
-  <script type="text/javascript">
-    (function($) {
-
-      /* Setting up smart banner close event handling */
-      $(".smart-banner-close").on("click", function() {
-        $(".smart-banner").hide();
-        Cookies.set("smartBannerHidden", true, {
-          expires: 1
-        });
-      });
-
-    })(jQuery);
-  </script>
-
-
-
-  <script type="text/javascript">
-    var ZipJet = ZipJet || {};
-    ZipJet.Frontend = ZipJet.Frontend || {};
-    ZipJet.Frontend.Customer = {
-      isNew: true
-    };
-    ZipJet.Frontend.Data = {
-      /**
-       * @type {string} locationIdentifier
-       */
-      locationIdentifier: 'fr_paris',
-      /**
-       * @type {string} languageCode
-       */
-      languageCode: 'fr',
-      /**
-       * @type {string} currencySymbol
-       */
-      currencySymbol: '&euro;',
-      /**
-       * @type {string} priceDecimalSeparator
-       */
-      priceDecimalSeparator: ',',
-      /**
-       * @type {bool} validateStreetNumber
-       */
-      validateStreetNumber: true,
-      /**
-       * @type {string} defaultLatLng
-       */
-      defaultLatLng: '48.8656366, 2.319047'
-    };
-
-    window.onload = function() {
-      var urls = [];
-
-      urls.push('https://app.adjust.com/dvthcv?deeplink=ZIPJET://fr/c&fallback=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.zipjet.android%26hl%3Dfr');
-      urls.push('https://app.adjust.com/kx5wdk?deeplink=ZIPJET://fr/c&fallback=https%3A%2F%2Fitunes.apple.com%2Ffr%2Fapp%2Fzipjet-dry-cleaning-laundry%2Fid926955591');
-
-      var $links = $('a').filter(function() {
-        return urls.indexOf($(this).attr('href')) > -1;
-      });
-
-      $links.attr('target', null);
-      $links.click(function(evt) {
-        evt.stopPropagation();
-        window.location.href = $(this).attr('href');
-        return false;
-      });
-    }
-  </script>
-
-  <div class="cookie-info">
-    En continuant à naviguer sur le site, vous acceptez l'utilisation de cookies afin de vous proposer des services et une offre adaptés à vos centres d'intérêts.
-    <a href="/politique-de-cookies">En savoir plus.</a>
-    <div class="close icon-ico-close"><i class="fa fa-times-circle"></i></div>
-  </div>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="/js/index.js" type="text/javascript"></script>
-  <script type="text/javascript">
-    window.NREUM || (NREUM = {});
-    NREUM.info = {
-      "beacon": "bam.nr-data.net",
-      "licenseKey": "15af52e329",
-      "applicationID": "30871199",
-      "transactionName": "ZV1aYRRVWktYABJeC1wXe0AVQFtVFgoIUwFKF1FbAlFM",
-      "queueTime": 0,
-      "applicationTime": 108,
-      "atts": "SRpZF1xPSUU=",
-      "errorBeacon": "bam.nr-data.net",
-      "agent": ""
-    }
-  </script>
-
-  <div id="fancybox-tmp"></div>
-  <div id="fancybox-loading">
-    <div></div>
-  </div>
-  <div id="fancybox-overlay"></div>
-  <div id="fancybox-wrap">
-    <div id="fancybox-outer">
-      <div class="fancybox-bg" id="fancybox-bg-n"></div>
-      <div class="fancybox-bg" id="fancybox-bg-ne"></div>
-      <div class="fancybox-bg" id="fancybox-bg-e"></div>
-      <div class="fancybox-bg" id="fancybox-bg-se"></div>
-      <div class="fancybox-bg" id="fancybox-bg-s"></div>
-      <div class="fancybox-bg" id="fancybox-bg-sw"></div>
-      <div class="fancybox-bg" id="fancybox-bg-w"></div>
-      <div class="fancybox-bg" id="fancybox-bg-nw"></div>
-      <div id="fancybox-content"></div><a id="fancybox-close"></a>
-      <div id="fancybox-title"></div><a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a><a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a>
-    </div>
-  </div>
-  <div class="pac-container pac-logo" style="display: none; width: 410px; position: absolute; left: 343px; top: 335px;"></div>
-  <div class="pac-container pac-logo" style="display: none; width: 362px; position: absolute; left: 770px; top: 2706px;"></div><iframe name="_hjRemoteVarsFrame" title="_hjRemoteVarsFrame" id="_hjRemoteVarsFrame" src="https://vars.hotjar.com/box-90f3a29ef7448451db5af955688970d7.html"
-    style="display: none !important; width: 1px !important; height: 1px !important; opacity: 0 !important; pointer-events: none !important;"></iframe>
-  <div id="ads"></div>
-
-  <script type="text/javascript" id="">
-    setTimeout(function() {
-      var a = document.createElement("script"),
-        b = document.getElementsByTagName("script")[0];
-      a.src = document.location.protocol + "//script.crazyegg.com/pages/scripts/0027/4449.js?" + Math.floor((new Date).getTime() / 36E5);
-      a.async = !0;
-      a.type = "text/javascript";
-      b.parentNode.insertBefore(a, b)
-    }, 1);
-  </script>
-  <div id="om-jki8gye7rwx9iamuz0dq-holder"></div>
-  <script type="text/javascript" id="">
-    var jki8gye7rwx9iamuz0dq, jki8gye7rwx9iamuz0dq_poll = function() {
-      var b = 0;
-      return function(d, c) {
-        clearInterval(b);
-        b = setInterval(d, c)
-      }
-    }();
-    ! function(b, d, c) {
-      if (b.getElementById(c)) jki8gye7rwx9iamuz0dq_poll(function() {
-        if (window.om_loaded && !jki8gye7rwx9iamuz0dq) return jki8gye7rwx9iamuz0dq = new OptinMonsterApp, jki8gye7rwx9iamuz0dq.init({
-          u: "23221.665751",
-          staging: 0,
-          dev: 0,
-          beta: 0
-        })
-      }, 25);
-      else {
-        var e = !1,
-          a = b.createElement(d);
-        a.id = c;
-        a.src = "https://a.optmstr.com/app/js/api.min.js";
-        a.async = !0;
-        a.onload = a.onreadystatechange = function() {
-          if (!(e || this.readyState && "loaded" !== this.readyState && "complete" !== this.readyState)) try {
-            e = om_loaded = !0, jki8gye7rwx9iamuz0dq =
-              new OptinMonsterApp, jki8gye7rwx9iamuz0dq.init({
-                u: "23221.665751",
-                staging: 0,
-                dev: 0,
-                beta: 0
-              }), a.onload = a.onreadystatechange = null
-          } catch (f) {}
-        };
-        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(a)
-      }
-    }(document, "script", "omapi-script");
-  </script>
 </body>
 </html>
