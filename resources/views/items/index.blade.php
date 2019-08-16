e96722508eae2a275e8448480d48223fb9eb2438
aouman <37551703+aouman@users.noreply.github.com>
2019-08-16 15:22:36 +0000

    remise en place de la page entreprise

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -17,9 +17,9 @@
                 <li data-tab="tab_drycleaning" class="active">
                     Pressing &amp; Repassage <span></span>
                 </li>
-                <!--li data-tab="tab_washnfold" class="">
+                <li data-tab="tab_washnfold" class="">
                     Linge Nettoyé &amp; Plié <span></span>
-                </li-->
+                </li>
             </ul>
         </div>
 
@@ -102,35 +102,35 @@
                     <p>Tarifé par kilo, notre service de linge au kilo convient parfaitement pour votre linge quotidien. Vos articles seront lavés ensemble à 30°C, séchés au sèche-linge puis pliés. Chaque sac de linge est tarifé au
                         minimum de 3 kg.</p>
                     <ul class="list">
-                        <li data-id="washnfold_0" data-product-reference="FR-PRO-B7431778" class="                additional-desc" style="min-height: 365px;">
+                      @foreach($items as $item)
+                        <li data-id="drycleaning_2" data-product-reference="FR-PRO-L9457501" class="                " style="min-height: 285px;">
 
-                            <div class="flipper" style="min-height: 365px;">
+                            <div class="flipper" style="min-height: 285px;">
                                 <div class="front">
 
                                     <span class="additional-desc-open-icon">i</span>
-                                    <div class="img-container noselect allow-substraction">
+                                    <div class="img-container noselect">
                                         <span class="item-substract item-update">-</span>
-                                        <img src="https://res.cloudinary.com/zipjet/image/upload/q_auto:good/v1490638365/Bag_puifzm.jpg">
+                                        <img src="/images/items/{{$item->image}}">
                                         <span class="item-add item-update">+</span>
                                     </div>
 
 
                                     <div class="name">
-                                        <span>Linge au Kilo (par kg avec minimum de 3kg par sac)</span>
+                                        <span>{{$item->name}}</span>
                                     </div>
-                                    <div class="description">(arrondi au kg supérieur)</div>
-                                    <div class="price" data-price="4.99">
+                                    <div class="price" data-price="19">
 
 
-                                        4,99€ </div>
+                                        <span>{{$item->price}}</span> <span>FCFA</span> </div>
                                 </div>
                                 <div class="back">
                                     <span class="additional-desc-close-icon">+</span>
-                                    <p>Il est important de noter que lorsque nous recevons les sacs de linge au kilo, ils sont pesés, lavés, séchés et pliés. Nous n'inspectons pas les étiquettes d'entretien de chaque article contenu
-                                        dans les sacs et nous ne sommes pas responsables si les instructions d'entretien ne correspondent pas à notre méthode de lavage et séchage. Le séchage se fait à 70°.</p>
+                                    <p></p>
                                 </div>
                             </div>
                         </li>
+                      @endforeach
                     </ul>
                 </div>
 
