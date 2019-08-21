9c80d56fa4a09db598b61c604249af7de3eac933
daviidy <yaodavidarmel@gmail.com>
2019-08-21 18:18:00 +0000

    correction fichier index de items

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.menu-list-items')
-
+​
 @section('content')
-
+​
 <section class="price-estimator" id="B2C-price-estimator">
-
+​
     <div class="price-estimator-holder content-holder">
-
+​
         <div class="price-estimator-header">
             <div class="headline-holder">
                 <h1>Tarifs pressing et linge</h1>
             </div>
             <input name="search" placeholder="Rechercher un article…" type="text" data-list=".list" class="item-search">
-
+​
             <!-- Categories tabs -->
             <ul id="category-tabs">
                 @foreach($categories as $category)
@@ -19,109 +19,77 @@
                 <li data-tab="tab_drycleaning" class="active">
                   <a href="#tab_{{preg_replace('/\\s/','',$category->name)}}" rel="nofollow">{{$category->name}} <span></span></a>
                 </li>
-
                     @endif
                 @endforeach
             </ul>
         </div>
-
+​
         <div class="content" id="price-estimator-content">
             <!-- Items in each tabs -->
             <h2>CHOISISSEZ VOS ARTICLES !</h2>
-
+​
             <div id="category-content" class="category-content">
                 <!-- Output each category in respective tab -->
                 @foreach($categories as $category)
                     @if($category->items->count() > 0)
-
+​
                 <div class="category-items " id="tab_{{preg_replace('/\\s/','',$category->name)}}" style="display:{{$loop->first ? 'block' : 'none'}};">
                     <ul class="list">
                         @foreach($category->items as $item)
                         <li data-id="drycleaning_2" data-product-reference="FR-PRO-L9457501" class="" style="min-height: 285px;">
-
+​
                             <div class="flipper" style="min-height: 285px;">
                                 <div class="front">
-
+​
                                     <span class="additional-desc-open-icon">i</span>
                                     <div class="img-container noselect">
                                         <span class="item-substract item-update">-</span>
                                         <img src="/images/items/{{$item->image}}">
                                         <span class="item-add item-update">+</span>
                                     </div>
-
-
+​
+​
                                     <div class="name">
                                         <span>{{$item->name}}</span>
                                     </div>
                                     <div class="price" data-price="19">
-
-
+​
+​
                                         <span>{{$item->price}}</span> <span>FCFA</span> </div>
                                 </div>
-
+​
                             </div>
                         </li>
                         @endforeach
                     </ul>
                 </div>
-                <div class="category-items" id="tab_washnfold" style="display: none;">
-                    <p>Tarifé par kilo, notre service de linge au kilo convient parfaitement pour votre linge quotidien. Vos articles seront lavés ensemble à 30°C, séchés au sèche-linge puis pliés. Chaque sac de linge est tarifé au
-                        minimum de 3 kg.</p>
-                    <ul class="list">
-                      @foreach($items as $item)
-                        <li data-id="drycleaning_2" data-product-reference="FR-PRO-L9457501" class="                " style="min-height: 285px;">
-
-                            <div class="flipper" style="min-height: 285px;">
-                                <div class="front">
-
-                                    <span class="additional-desc-open-icon">i</span>
-                                    <div class="img-container noselect">
-                                        <span class="item-substract item-update">-</span>
-                                        <img src="/images/items/{{$item->image}}">
-                                        <span class="item-add item-update">+</span>
-                                    </div>
-
-
-                                    <div class="name">
-                                        <span>{{$item->name}}</span>
-                                    </div>
-                                    <div class="price" data-price="19">
-
-
-                                        <span>{{$item->price}}</span> <span>FCFA</span> </div>
-                                </div>
-                                <div class="back">
-                                    <span class="additional-desc-close-icon">+</span>
-                                    <p></p>
-                                </div>
-                            </div>
-                        </li>
-                      @endforeach
-                    </ul>
-                </div>
+​
                 @endif
                 @endforeach
-
+​
+​
+​
+​
             </div>
         </div>
-
+​
         <div class="right-column fixed-summary">
             <div id="summary-container">
-
+​
                 <table class="bottom-line">
                     <tbody>
                         <tr>
                             <td class="total-price-label">
                                 <span>
                                     Total <!--i>(TVA incluse)</i-->
-
-
-
-
+​
+​
+​
+​
                                 </span>
                             </td>
                             <td class="total-price-value">
-
+​
                                 @if(session('orders'))
                                 @php
                                 $price = 0;
@@ -141,11 +109,11 @@
                         </tr>
                     </tbody>
                 </table>
-
+​
                 <div class="scrollable">
                     <table id="summary-items">
-
-
+​
+​
                         <tbody>
                             @if(session('orders'))
                             @foreach(session('orders') as $order)
@@ -156,11 +124,11 @@
                             </tr>
                             @endforeach
                             @endif
-
+​
                         </tbody>
                     </table>
                 </div>
-
+​
               <p class="disclaimer"><sup>*</sup>Veuillez noter que le prix definitif pourra être fixé légèrement à la hausse ou à la baisse  par notre pressing partenaire. Le prix total peut varier en conséquence.
                     <style>
                         #nav ul li:first-of-type, .header-holder .order-button { display: none; }
@@ -168,55 +136,55 @@ section.price-estimator .price-estimator-holder .content .category-content .cate
 section.price-estimator .price-estimator-holder .content .category-content .category-items ul li.flip .back p { display: block !important; }
 </style>
 </p>
-
+​
                 @auth
                 <a class="order-btn-map-view" id="orderNowLink">
                     Commandez maintenant </a>
                 <div style="clear:both;"></div>
                 @endauth
                 @guest
-
+​
                 <a href="#" data-toggle="modal" data-target="#inscription" class="order-btn-map-view">
                     Commandez maintenant </a>
                 <div style="clear:both;"></div>
-
+​
                 @endguest
-
-
+​
+​
             </div>
         </div>
-
+​
         <div style="clear:both"></div>
-
+​
     </div>
-
-
-
-
-
+​
+​
+​
+​
+​
 </section>
 <script type="text/javascript">
 $('#category-tabs a').on('click', function (event) {
     event.preventDefault();
-
+​
     $('.active').removeClass('active');
     $(this).parent().addClass('active');
     $('.category-content .category-items').hide();
     $($(this).attr('href')).show();
 });
-
+​
 $('#category-tabs a:first').trigger('click'); // Default
-
+​
 </script>
-
+​
 @include('includes.items_page.registration')
-
+​
 @include('includes.items_page.login')
-
-
+​
+​
 @include('includes.items_page.popupOrange')
-
-
-
-
+​
+​
+​
+​
 @endsection
