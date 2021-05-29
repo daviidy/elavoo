5ebcfade4d6d370dc6cd1e940dd7ccce9c798720
Sana Michael Yves <sana.michael120@gmail.com>
2021-05-29 14:44:22 +0000

    - Add Home page link on reset password view - Add resend mail feature on above activation account view - Add Home page link on activation account view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Auth;
 use Image;
+use Illuminate\Support\Str;
+use App\Mail\SendEmailVerification;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -25,7 +28,63 @@ class UserController extends Controller
 
     public function account_activation()
     {
+        $user = auth()->user();
+        if ($user && !is_null($user->email_verified_at)) {
+            return redirect()->route('home');
+        }
         return view('auth.activate-account');
+    }
+
+    public function send_account_activation()
+    {
+        return view('auth.send-activation-code');
+    }
+
+    public function send_account_activation_mail(Request $request)
+    {
+        // - Validation de mail
+        $this->validate($request, [
+            'email' => ['required', 'string', 'email'],
+        ],[
+            'email.required' => "L'adresse email est obligatoire",
+            'email.string' => "L'adresse email est obligatoire",
+            'email.email' => "L'adresse email saisie est invalide",
+        ]);
+        // - Vérifier l'existence du compte
+        $user = User::where(['email' => $request->email])->exists();
+        if (!$user) {
+            $request->session()->flash('error', "Ce compte est introuvable, veuillez vous réinscrire.");
+            return view('auth.send-activation-code');
+        }
+        // - Create email verification
+        $token = $this->getGeneratedUniqueToken();
+        // - Send email
+        try {
+            // - Sending email
+            Mail::to( $request->email)->send(new SendEmailVerification(['token' => $token]));
+            DB::table('emails_verifications')->where('email', $request->email)->delete();
+            DB::table('emails_verifications')->insert([
+                'email' =>  $request->email,
+                'token' => $token,
+            ]);
+        } catch (\Exception $e) {
+            // - Problem occurs
+            $request->session()->flash('error', "Une erreur est survenue lors de l'envoi du mail, veuillez réessayer");
+            return redirect()->back()->withInput(['email' => $request->email]);
+        }
+
+        $request->session()->flash('success', "Nous avons envoyé un mail d'activation de compte dans votre boîte mail.");
+
+        return view('auth.send-activation-code');
+    }
+
+    public function getGeneratedUniqueToken() {
+        do {
+            $token = Str::random(60);
+        } while(
+            DB::table('emails_verifications')->where('token',  $token)->exists()
+        );
+        return $token;
     }
 
     public function account_activate(Request $request, $token)
